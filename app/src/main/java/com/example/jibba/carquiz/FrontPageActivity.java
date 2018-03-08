20c3533a7a5d2d954e13f8b6d40230d51b6b1312
jibauch94 <jib090994@gmail.com>
2018-03-08 12:18:04 +0100

    Appbar created and replaces the old one With spinner and two items/buttons

--- app/src/main/java/com/example/jibba/carquiz/FrontPageActivity.java
+++ app/src/main/java/com/example/jibba/carquiz/FrontPageActivity.java
@@ -2,12 +2,55 @@ package com.example.jibba.carquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class FrontPageActivity extends AppCompatActivity {
+
+    Spinner helpDropDown;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
+
+        helpDropDown = findViewById(R.id.helpDropDown);
+        adapter = ArrayAdapter.createFromResource(this, R.array.settingSelection, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        helpDropDown.setAdapter(adapter);
+        helpDropDown.setOnItemSelectedListener(dropDownListener);
+
+
+
+}
+
+    private AdapterView.OnItemSelectedListener dropDownListener = new AdapterView.OnItemSelectedListener()
+    {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String aboutTheGame = "About The Game";
+            String rules = "Game Rules";
+
+            if (parent.getItemAtPosition(position).equals(aboutTheGame)) {
+                toastMessage(aboutTheGame + " valgt");
+
+            } else if (parent.getItemAtPosition(position).equals(rules)) {
+                toastMessage(rules + " valgt");
+            }
+
+        }
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+
+    };
+    private void toastMessage(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
